fcdeeaa61c5c9b5f0e07137fbbdf6f00bf2540ea
defitjo <defitjo16@gmail.com>
2022-03-06 17:42:15 -0500

    portfolio

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// sidenav
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+// scrollspy
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".scrollspy");
+  var instances = M.ScrollSpy.init(elems, options);
+});
+
+// div animation
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
